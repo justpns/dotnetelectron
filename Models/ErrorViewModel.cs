0dccb32abf7282affd25995a31d919b4450459db
PanusornS <panusorn@genxas.com>
2018-08-31 09:46:58 +0700

    Init mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetElectron.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
